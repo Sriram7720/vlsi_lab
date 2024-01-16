281177e561643f30e3ebf1dea011793138792475
Sriram7720 <108873236+Sriram7720@users.noreply.github.com>
2024-01-16 19:57:44 +0530

    Create test_bench.v

---
+++ dlatch/test_bench.v
@@ -0,0 +1,20 @@
+module test;
+  reg d,en,rst;
+  wire q,qb;
+  dlatch d1(d,en,rst,q,qb);
+  
+  initial 
+    begin
+      $dumpfile("dump.vcd"); 
+      $dumpvars;
+      d = 0;
+      en = 0;
+      rst = 0;
+    end
+  always #8 en = ~en;
+  always #5 d = ~d;
+  initial
+  #30 rst = 1;
+  initial
+  #120 $finish;
+endmodule
